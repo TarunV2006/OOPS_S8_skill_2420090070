4f524c5c3ab848c53df2e1a16c68c07375f1876a
TarunV2420090070 <2420090070@klh.edu.in>
2025-07-30 11:35:55 +0530

    Add files via upload

---
+++ OOP_SKILL_SESSIONS/src/Student_Dashboard.java
@@ -0,0 +1,17 @@
+public class Student_Dashboard {
+    public static void main(String[] args) {
+        long rollNumber = 2420090070l;
+        String name = "Tarun";
+        float marks = 85.75f;
+        char gender = 'M';
+        boolean isPass = true;
+
+        System.out.println("------ Student Summary Slip ------");
+        System.out.println("Roll Number   : " + rollNumber);
+        System.out.println("Name          : " + name);
+        System.out.println("Marks         : " + marks);
+        System.out.println("Gender        : " + gender);
+        System.out.println("Pass Status   : " + (isPass ? "Pass" : "Fail"));
+        System.out.println("----------------------------------");
+    }
+}
